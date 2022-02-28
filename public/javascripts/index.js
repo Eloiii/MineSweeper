e0df588c9c52cb8e12863e4c53d599aa1459beae
Eloiii <eloi.charra@mail.com>
2022-02-28 09:00:43 +0100

    calculating numbers (not good)

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,10 +1,17 @@
 const ROW = 10
 const COL = ROW
 const BOMBS = (10 / 100) * (ROW * COL)
+const BOMB = "BOMB"
+const UNKNOWN = "UNKNOWN"
+let gameOver = false
 
 const gridOBJ = document.querySelector(".grid");
 let grid = new Array(ROW);
 
+
+initGrid()
+console.log(grid)
+initCSS()
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
@@ -14,40 +21,75 @@ function getBombsLocation() {
     let res = []
     let bombRemaining = BOMBS
     while(bombRemaining > 0) {
-        console.log(bombRemaining)
-        const row = getRandomInt(ROW)
-        const col = getRandomInt(COL)
-        console.log({row: row, col: col})
+        let row, col
+        do {
+            row = getRandomInt(ROW)
+            col = getRandomInt(COL)
+        } while(isABomb(row, col, res))
         res.push({row: row, col: col})
         bombRemaining--
     }
     return res
 }
 
-function isABombLocation(row, col, bombs) {
-    for (let i = 0; i < BOMBS; i++) {
+function isABomb(row, col, bombs) {
+    for (let i = 0; i < bombs.length; i++) {
         if(bombs[i].row === row && bombs[i].col === col)
             return true
     }
     return false
 }
 
-function initGrid() {
-    const bombs = getBombsLocation()
+function setBombs(bombs) {
     for (let row = 0; row < ROW; row++) {
         grid[row] = new Array(COL)
         for (let col = 0; col < COL; col++) {
-            if (isABombLocation(row,col,bombs))
-                grid[row][col] = "BOMB"
+            if (isABomb(row, col, bombs))
+                grid[row][col] = BOMB
             else
-                grid[row][col] = "UNKNOWN"
+                grid[row][col] = UNKNOWN
         }
     }
 }
 
-initGrid()
-console.log(grid)
-initCSS()
+function getDistance(row, col, rowBomb, colBomb) {
+    const dx = Math.abs(rowBomb - row);
+    const dy = Math.abs(colBomb - col);
+
+    const min = Math.min(dx, dy);
+    const max = Math.max(dx, dy);
+
+    const diagonalSteps = min;
+    const straightSteps = max - min;
+
+    return Math.round(Math.sqrt(2) * diagonalSteps + straightSteps)
+}
+
+function getDistanceFromClosestBomb(row, col, bombs) {
+    let closest = Number.MAX_SAFE_INTEGER
+    for (const bomb of bombs) {
+        let distance = getDistance(row, col, bomb.row, bomb.col)
+        if(distance < closest)
+            closest = distance
+    }
+    return closest
+}
+
+function setNumbers(bombs) {
+    for (let row = 0; row < ROW; row++) {
+        for (let col = 0; col < COL; col++) {
+            if(grid[row][col] !== BOMB) {
+                grid[row][col] = getDistanceFromClosestBomb(row, col, bombs)
+            }
+        }
+    }
+}
+
+function initGrid() {
+    const bombs = getBombsLocation()
+    setBombs(bombs)
+    setNumbers(bombs)
+}
 
 function initCSS() {
     clearGrid()
@@ -57,9 +99,9 @@ function initCSS() {
         DIVRow.style.display = "flex";
         for (let col = 0; col < COL; col++) {
             let DIVCell = document.createElement("div")
-            // DIVCell.textContent = "💣"
+            // DIVCell.textContent = grid[row][col] === BOMB ? "💣" : grid[row][col]
             DIVCell.className = "cell"
-            DIVCell.addEventListener("click", cellClick)
+            DIVCell.addEventListener("click", revealCell)
             DIVCell.param = {row: row, col: col}
             DIVRow.appendChild(DIVCell)
         }
@@ -67,12 +109,19 @@ function initCSS() {
     }
 }
 
-function cellClick(evt) {
-    console.log(evt.currentTarget.param)
-}
 
 function clearGrid() {
     while (gridOBJ.firstChild) {
         gridOBJ.removeChild(gridOBJ.firstChild);
+    }
+}
+
+function revealCell(evt) {
+    const row = evt.currentTarget.param.row
+    const col = evt.currentTarget.param.col
+    if(grid[row][col] === BOMB) {
+        evt.currentTarget.textContent = "💣"
+    } else {
+        evt.currentTarget.textContent = grid[row][col]
     }
 }
